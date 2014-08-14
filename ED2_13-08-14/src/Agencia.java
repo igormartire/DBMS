14c2a268d4488600f3164d7b855e09515fc5739a
Lucas <Lucas@Lucas-PC.wifi.uff.br>
2014-08-13 22:15:17 -0300

    Alterado Agencia.Salva()

--- ED2_13-08-14/src/Agencia.java
+++ ED2_13-08-14/src/Agencia.java
@@ -4,8 +4,6 @@ import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /*
  * To change this template, choose Tools | Templates
