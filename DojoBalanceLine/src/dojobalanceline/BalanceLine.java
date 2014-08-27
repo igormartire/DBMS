60eafdc3dfd14236d55502e898333df2744b2607
gcrsaldanha <gabrielcrsaldanha@gmail.com>
2014-08-26 22:30:19 -0300

    Adicionados comentários
    Esse commit pode ser revertido, mas avise caso algum comentário esteja
    errado :panda_face:

--- DojoBalanceLine/src/dojobalanceline/BalanceLine.java
+++ DojoBalanceLine/src/dojobalanceline/BalanceLine.java
@@ -33,44 +33,55 @@ public class BalanceLine {
         itemTransacao = Transacao.le(inTransacao);
         itemMestre = Cliente.le(inMestre);
 
+        // Enquanto nenhum dos dois atingiu o fim do arquivo
         while (itemTransacao.chave != Integer.MAX_VALUE
                 && itemMestre.codCliente != Integer.MAX_VALUE) {
 
+            // Se a chave do itemMestre for menor, vai salvando direto no arquivo de saida seus valores
             if (itemMestre.codCliente < itemTransacao.chave) {
                 itemMestre.salva(outSaida);
                 itemMestre = Cliente.le(inMestre);
-
+            // Se for igual..
             } else if (itemMestre.codCliente == itemTransacao.chave) {
 
                 switch (itemTransacao.tipoTransacao) {
+                    // Caso seja insercao, vai salvar o ERRO E o mestre
                     case Transacao.INSERCAO:
                         itemMestre.salva(outSaida);
                         itemTransacao.salva(outErro);
                         break;
+                    // Caso exclusao, apenas nao salva no arquivo de saida (mestre)
                     case Transacao.EXCLUSAO:
                         break;
+                    // Caso seja modificacao, solicita os valores
                     case Transacao.MODIFICACAO:
 
                         TransacaoModificacao mod = (TransacaoModificacao) itemTransacao;
-
+                        // So tem 2 atributos a serem mudados e estamos considerando que 
+                        // o arquivo transacao esta correto (nao tem atributos que nao existem)
                         if (mod.nomeAtributo.equals("nome")) {
                             itemMestre.nome = mod.valorAtributo;
                         } else {
                             itemMestre.dataNascimento = mod.valorAtributo;
                         }
+                        // Depois de alterar o atributo solicitado, salva.
                         itemMestre.salva(outSaida);
 
                         break;
                 }
+                // Le o proximo itemMestre E itemTransacao
                 itemMestre = Cliente.le(inMestre);
                 itemTransacao = Transacao.le(inTransacao);
-            } else {
+            } else {    // Caso codCliente seja maior que itemTransacao.chave
+                        // ou seja, tem que executar a transacao antes
                 switch (itemTransacao.tipoTransacao) {
                     case Transacao.INSERCAO:
                         TransacaoInsercao ins = (TransacaoInsercao) itemTransacao;
+                        // Instancia um cliente com os dados vindos do arquivo de transacao
                         Cliente novoCliente = new Cliente(ins.chave, ins.nomeCliente, ins.dataNascimento);
                         novoCliente.salva(outSaida);
                         break;
+                    // Erro, pois a chave e diferente
                     case Transacao.EXCLUSAO:
                         itemTransacao.salva(outErro);
                         break;
@@ -78,18 +89,21 @@ public class BalanceLine {
                         itemTransacao.salva(outErro);
                         break;
                 }
+                // Le APENAS a proxima TRANSACAO
                 itemTransacao = Transacao.le(inTransacao);
             }
 
         }
-
+        
+        // So chega nesse ponto quando um dos dois (ou os dois) arquivos acabarem
+        // Se foi o de transacao que acabou, apenas salva todo o resto do itemMestre no out
         if (itemTransacao.chave == Integer.MAX_VALUE) {
             while (itemMestre.codCliente != Integer.MAX_VALUE) {
                 itemMestre.salva(outSaida);
                 itemMestre = Cliente.le(inMestre);
 
             }
-        } else {
+        } else {    // Caso ainda existam transacoes a serem realizadas
             while (itemTransacao.chave != Integer.MAX_VALUE) {
                 switch (itemTransacao.tipoTransacao) {
                     case Transacao.INSERCAO:
@@ -108,7 +122,8 @@ public class BalanceLine {
                 itemTransacao = Transacao.le(inTransacao);
             }
         }
-
+        
+        // Salva o valor convencionado para determinar fim de arquivo
         Cliente maxCliente = new Cliente(Integer.MAX_VALUE, "    ", "          ");
         maxCliente.salva(outSaida);
         Transacao maxTrans = new TransacaoExclusao(Integer.MAX_VALUE);
